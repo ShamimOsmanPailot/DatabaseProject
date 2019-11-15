0510ed32df34d047e1a61b6f005f1a942d6963fb
ShamimOsmanPailot <cse.shamimosmanpailot@gmail.com>
2019-11-15 10:44:53 +0600

    Welcome

---
+++ Railway Reservation/Railway_Table_Create-Database.sql
@@ -0,0 +1,85 @@
+create database Railway_Reservation;
+
+create table Create_A/C(
+
+);
+
+
+create table user_information(
+
+);
+
+
+create table Passenger_information(
+
+
+);
+
+
+create table Train_Difference(
+
+
+
+);
+
+
+create table Train_information(
+
+
+
+);
+
+
+create table Train_Route(
+
+
+
+);
+
+
+create table Seat_information(
+
+
+
+);
+
+
+create table Station_information(
+
+
+);
+
+
+create table Station_Distance(
+
+
+
+);
+
+
+create table Ticket_information(
+
+
+
+);
+
+
+create table Payment_Method(
+
+
+
+);
+
+
+create table Train_Time_Table(
+
+
+
+);
+
+
+create table Contact_information(
+
+
+
+);
---
+++ Railway Reservation/Railway_insert_value_Database.sql
@@ -0,0 +1,102 @@
+select * from Create_A/C
+insert into ()
+values(
+
+
+);
+
+
+select * from user_information
+insert into ()
+values(
+
+
+);
+
+
+select * from Passenger_information
+insert into ()
+values(
+
+
+);
+
+
+select * from Train_Difference
+insert into ()
+values(
+
+
+);
+
+
+select * from Train_information
+insert into ()
+values(
+
+
+);
+
+
+select * from Train_Route
+insert into ()
+values(
+
+
+);
+
+
+select * from Seat_information
+insert into ()
+values(
+
+
+);
+
+
+select * from Station_information
+insert into ()
+values(
+
+
+);
+
+
+select * from Station_Distance
+insert into ()
+values(
+
+
+);
+
+
+select * from Ticket_information
+insert into ()
+values(
+
+
+);
+
+
+select * from Payment_Method
+insert into ()
+values(
+
+
+);
+
+
+select * from Train_Time_Table
+insert into ()
+values(
+
+
+);
+
+
+select * from Contact_information
+insert into ()
+values(
+
+
+);
